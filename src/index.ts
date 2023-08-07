de1d3a60f088751f855fbc5fa7a572424792f366
dimasekaputra2000 <dimasekaputra2000@gmail.com>
2023-08-07 10:20:53 +0700

    (fix) missing next function on cors handler

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
 });
 
 app.use('/hello', (req: Request, res: Response, next: NextFunction) => {
